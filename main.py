943f4737da7314fdcb4ad6e550871adb4f15855d
gelu <1152596761@qq.com>
2024-12-07 00:30:29 +0800

    add a person

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import os
 def hello_world():
     print('hello world!')
 
+class Person():
+    def __init__(self):
+        pass
+    def move():
+        print("walking")
 
 if __name__ == '__main__':
     hello_world()
